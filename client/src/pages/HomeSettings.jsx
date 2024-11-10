8b986fdcfe12bdc9a65294be126edf2a534fdbea
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2024-11-10 23:14:45 +0600

    chore: Remove toast message from HomeSettings page

--- client/src/pages/HomeSettings.jsx
+++ client/src/pages/HomeSettings.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react'
 import DashboardLayout from '../components/layout/DashboardLayout'
-import toast from 'react-hot-toast'
 import HomePageForm from '../components/HomePage/HomepageForm'
 
 const HomeSettings = () => {
-        toast.success("Welcome to the Home Settings page")
   return (
     <DashboardLayout>
         <main className="p-3">
